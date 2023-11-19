b630b5d825063702de7ae7f4c7693a2a416e5ff4
qaezz <russian.devv@gmail.com>
2023-11-18 20:02:21 -0600

    fixed logger.py when running main.py

--- src/tests/run_tests.py
+++ src/tests/run_tests.py
@@ -1,13 +1,13 @@
 import os
 import sys
 import unittest
-from logger import Logger
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-else:
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+
+from logger import Logger
 from test_db import TestDb
 from test_setup_db import TestSetupDB
 from test_trader import TestTrader
